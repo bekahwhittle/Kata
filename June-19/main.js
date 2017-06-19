d6549684f10172e61c8eba387308626b27fa2fec
teambekbek <whittlebekah@gmail.com>
2017-06-19 12:05:57 -0500

    got the button going up in increments of 1

--- June-19/main.js
+++ June-19/main.js
@@ -1,2 +1,10 @@
 
-console.log("HELLO WORLD");
+var btnCounter = document.getElementById('counter');
+btnCounter.innerHTML = 0;
+
+btnCounter.addEventListener('click', function buttonClick () {
+  btnCounter.innerHTML ++;
+  console.log("HELLO WORLD");
+})
+
+console.log(btnCounter);
