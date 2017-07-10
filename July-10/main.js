2514c2c1c6bdde04c3f091dced1e6292c2879377
teambekbek <whittlebekah@gmail.com>
2017-07-10 16:11:30 -0500

    understood that while loops continue until the conditions/parameters are false

--- July-10/main.js
+++ July-10/main.js
@@ -2,33 +2,23 @@
 // returns its multiplicative persistence, which is the number of times you must
 // multiply the digits in num until you reach a single digit.
 
-
 var count = 0;
 
 function persistence(num) {
-
   function multiply (int) {
     return int.reduce(function (a, b) {
         return a * b;
       });
   }
-
   while(num.toString().length > 1) {
     num = num.toString().split("");
     num = multiply(num);
-    count ++
+    count ++;
   }
-
   return count;
-
 }
 
 console.log(persistence(30));
 console.log(persistence(39));
 console.log(persistence(4));
 console.log(persistence(999));
-
-var thirtyN = 400000.35;
-
-console.log(thirtyN);
-console.log(thirtyN.toString().split("")); // [ "4", "0", "0", "0", "0", "0", ".", "3", "5"]
