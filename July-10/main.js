842fe45655d41e8ff534a3ab808e941894bb7996
teambekbek <whittlebekah@gmail.com>
2017-07-10 15:23:50 -0500

    persistence multiplicative practice

--- July-10/main.js
+++ July-10/main.js
@@ -1,18 +1,39 @@
 // Write a function, persistence, that takes in a positive parameter num and
 // returns its multiplicative persistence, which is the number of times you must
 // multiply the digits in num until you reach a single digit.
-//
-// For example:
-
-// persistence(39) === 3 // because 3*9 = 27, 2*7 = 14, 1*4=4
-                       // and 4 has only one digit
-
- // persistence(999) === 4 // because 9*9*9 = 729, 7*2*9 = 126,
-                        // 1*2*6 = 12, and finally 1*2 = 2
-
- // persistence(4) === 0 // because 4 is already a one-digit number
 
 
- function persistence(num) {
-    //code me
- }
+var count = 0;
+
+function persistence(num) {
+
+  function multiply (int) {
+    return int.reduce(
+      function (a, b) {
+        return a * b;
+      }
+    );
+  }
+
+  while(num.toString().length > 1) {
+    num = num.toString().split("");
+    num = multiply(num);
+    count ++
+  }
+
+  return count;
+
+}
+
+console.log(persistence(30));
+console.log(persistence(39));
+console.log(persistence(4));
+console.log(persistence(999));
+
+
+function times (a, b) {
+  return a * b;
+}
+console.log(times(3,9)); // gives us 27
+console.log(times(2,7)); // gives us 14
+console.log(times(1,4)); // gives us 4 - persistence(39) == 4
