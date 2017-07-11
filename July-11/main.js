777fefbe5d36ff50361d4290a47e537c63fa72b5
teambekbek <whittlebekah@gmail.com>
2017-07-11 16:31:04 -0500

    working through new Kata today

---
+++ July-11/main.js
@@ -0,0 +1,23 @@
+// You probably know the "like" system from Facebook and other pages. People can
+// "like" blog posts, pictures or other items. We want to create the text that
+// should be displayed next to such an item.
+//
+// Implement a function likes :: [String] -> String, which must take in input
+// array, containing the names of people who like an item. It must return the
+// display text as shown in the examples:
+
+// likes [] // must be "no one likes this"
+// likes ["Peter"] // must be "Peter likes this"
+// likes ["Jacob", "Alex"] // must be "Jacob and Alex like this"
+// likes ["Max", "John", "Mark"] // must be "Max, John and Mark like this"
+// likes ["Alex", "Jacob", "Mark", "Max"] // must be "Alex, Jacob and 2 others
+// like this"
+console.log("hi");
+
+var friends = ["John", "Patrick", "Bekah", "Jamal"];
+if (friends => 1) {
+  console.log(friends + " liked this");
+}
+// while (friends.length => 1) {
+//   console.log("liked this");
+// }
